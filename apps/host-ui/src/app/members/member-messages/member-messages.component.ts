2f1c101c28663897a800f4ac12a3811060a523f9
shrestha1shreejan <sthasgn1@gmail.com>
2022-07-08 15:59:48 -0500

    adding scroll function for messages

--- apps/host-ui/src/app/members/member-messages/member-messages.component.ts
+++ apps/host-ui/src/app/members/member-messages/member-messages.component.ts
@@ -1,12 +1,13 @@
 import { NgForm } from "@angular/forms";
 import { MessageService } from "./../../_services/message.service";
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit, ViewChild } from '@angular/core';
 import { Message } from '../../_models/message';
 
 @Component({
+	changeDetection: ChangeDetectionStrategy.OnPush,
 	selector: 'app-member-messages',
 	templateUrl: './member-messages.component.html',
-	styleUrls: ['./member-messages.component.css']
+	styleUrls: ['./member-messages.component.css']	
 })
 export class MemberMessagesComponent implements OnInit {
 	@ViewChild('messageForm') messageForm: NgForm;
